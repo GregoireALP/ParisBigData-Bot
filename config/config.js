0a9467b982fcd7a01431aae6e88737cc85229dfb
GregoireALP <gregoire.alperovitch@gmail.com>
2020-06-09 10:28:37 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    API_KEY: "BfWhWs0IeeyBIiEiX8gSP8CB5",
-    API_SECRET_KEY: "RhYfFf2rd5UBbwppoOBP9pmldOmqZ57d0D0iR0Chpn3DHohQXK",
-    ACCESS_TOKEN: "1265672548873879552-MbgzT4mNMmgYlLdC6JID95WtEpDUDU",
-    ACCESS_TOKEN_SECRET: "r2XA1p4rtop6kmDVycjGWUTBhIExdanKUTfnFaQL8hEEn",
+    API_KEY: "xxxxxxxxxxxxxxx",
+    API_SECRET_KEY: "xxxxxxxxxxxxxxxxxxxx",
+    ACCESS_TOKEN: "xxxxxxxxxxxxxxxxxxxx",
+    ACCESS_TOKEN_SECRET: "xxxxxxxxxxxxxxxxxxxxx",
 
     POST_TYPE: "statuses/update",
     USERNAME: "@DevNodeJS"
